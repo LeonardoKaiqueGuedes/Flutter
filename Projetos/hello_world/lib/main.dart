9b0c50ec9e5a193b5d83ffbe698cc57d86ef5a0b
Leonardo Kaique Rocha Guedes <leonardoguedes@brq.com>
2019-11-19 15:19:54 -0300

    Gerador de Nomes Aleatórios

--- Projetos/hello_world/lib/main.dart
+++ Projetos/hello_world/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,76 +7,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Olá, Mundo!',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: MyHomePage(title: 'Incremento e Decremento'),
+      title: 'Gerador de Nomes Aleatórios',            
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-   setState(() {
-     _counter--;
-   });
- }
-
+class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    appBar: AppBar(
+      title: Text('Gerador de Nomes Aleatórios'),
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-       mainAxisAlignment: MainAxisAlignment.end,
-       children: <Widget>[
-         FloatingActionButton(
-           onPressed: _incrementCounter,
-           tooltip: 'Increment',
-           child: Icon(Icons.add),
-         ),
-         SizedBox(
-           width: 10.0,
-         ),
-         FloatingActionButton(
-           onPressed: _decrementCounter,
-           tooltip: 'Decrement',
-           child: Icon(Icons.remove),
-         ),
-       ],
-     ),
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
