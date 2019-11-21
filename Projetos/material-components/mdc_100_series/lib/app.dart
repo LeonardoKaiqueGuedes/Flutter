73dced3cff4da5c4ba06798be28a77ff4ca8dc5e
Leonardo Kaique Rocha Guedes <leonardoguedes@brq.com>
2019-11-21 15:17:08 -0300

    Material Design

---
+++ Projetos/material-components/mdc_100_series/lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'login.dart';
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class ShrineApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
+      home: HomePage(),
+      // TODO: Make currentCategory field take _currentCategory (104)
+      // TODO: Pass _currentCategory for frontLayer (104)
+      // TODO: Change backLayer field value to CategoryMenuPage (104)
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+      // TODO: Add a theme (103)
+    );
+  }
+
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
---
+++ Projetos/material-components/mdc_100_series/lib/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Usuário',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Senha',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Cancelar'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Próximo'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
